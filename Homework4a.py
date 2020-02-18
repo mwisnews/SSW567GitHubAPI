a07136feb99daf0df39227172adfccaf6798bf84
mwisnews <mwisnews@stevens.edu>
2020-02-18 15:26:45 -0500

    Added main program file

---
+++ Homework4a.py
@@ -0,0 +1,25 @@
+import json
+import requests
+
+def getRepositories(gitHubUsername):
+    repoSummaryList = []
+    try:
+        url = "https://api.github.com/users/" + gitHubUsername + "/repos"
+    except TypeError:
+        return "Username must be entered as a string"
+    
+    repoInfo = requests.get(url)
+    JsonExtract = json.loads(repoInfo.content)
+
+    for i in JsonExtract:
+        commits = requests.get("https://api.github.com/repos/" + gitHubUsername + "/" + i['name'] + "/commits")
+        JsonCommits = json.loads(commits.content)
+        count = 0
+        for j in JsonCommits:
+            count = count + 1
+        repoSummaryList.append([i['name'], count])
+    return repoSummaryList
+
+
+listor = getRepositories("mwisnews")
+print(listor)

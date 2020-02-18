8e404b07aba525e387414c6250b4c3d28af3316c
mwisnews <mwisnews@stevens.edu>
2020-02-18 17:32:46 -0500

    Final Test Case Update

--- Homework4aTesting.py
+++ Homework4aTesting.py
@@ -4,10 +4,10 @@ import Homework4a
 class TestHomeworka(unittest.TestCase):
 
     def testWorkingUsernameA(self):
-        self.assertEqual(Homework4a.getRepositories("mwisnews"), [['BusinessCardReader', 30], ['MWSSW567', 2], ['SSW567GitHubAPI', 15], ['TexasHoldem', 15], ['TriangleTestingAssignment2', 10], ['TriangleTestingClassification', 1], ['VentureHacks-Fintech', 30]])
+        self.assertEqual(Homework4a.getRepositories("mwisnews"), [['BusinessCardReader', 30], ['MWSSW567', 2], ['SSW567GitHubAPI', 16], ['TexasHoldem', 15], ['TriangleTestingAssignment2', 10], ['TriangleTestingClassification', 1], ['VentureHacks-Fintech', 30]])
 
     def testWorkingUsernameB(self):
-        self.assertEqual(Homework4a.getRepositories("richkempinski"), [['hellogitworld', 50], ['helloworld', 6], ['Mocks', 10], ['Project1', 2], ['threads-of-life', 1]])
+        self.assertEqual(Homework4a.getRepositories("richkempinski"), [['hellogitworld', 30], ['helloworld', 6], ['Mocks', 10], ['Project1', 2], ['threads-of-life', 1]])
 
     def testInvalidInputA(self):
         self.assertEqual(Homework4a.getRepositories(True), "Username must be entered as a string")

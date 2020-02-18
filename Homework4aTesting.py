89149dbafb669f095541809ebd1d618e7fa9965f
mwisnews <mwisnews@stevens.edu>
2020-02-18 16:27:37 -0500

    Travis Verification

--- Homework4aTesting.py
+++ Homework4aTesting.py
@@ -4,7 +4,7 @@ import Homework4a
 class TestHomeworka(unittest.TestCase):
 
     def testWorkingUsernameA(self):
-        self.assertEqual(Homework4a.getRepositories("mwisnews"), [['BusinessCardReader', 30], ['MWSSW567', 2], ['SSW567GitHubAPI', 12], ['TexasHoldem', 15], ['TriangleTestingAssignment2', 10], ['TriangleTestingClassification', 1], ['VentureHacks-Fintech', 30]])
+        self.assertEqual(Homework4a.getRepositories("mwisnews"), [['BusinessCardReader', 30], ['MWSSW567', 2], ['SSW567GitHubAPI', 13], ['TexasHoldem', 15], ['TriangleTestingAssignment2', 10], ['TriangleTestingClassification', 1], ['VentureHacks-Fintech', 30]])
 
     def testWorkingUsernameB(self):
         self.assertEqual(Homework4a.getRepositories("richkempinski"), [['hellogitworld', 30], ['helloworld', 6], ['Mocks', 10], ['Project1', 2], ['threads-of-life', 1]])

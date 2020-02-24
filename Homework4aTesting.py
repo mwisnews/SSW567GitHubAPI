0eb709f2bdf7763289d60fa80a41222ee4ebb7b1
mwisnews <mwisnews@stevens.edu>
2020-02-24 12:12:33 -0500

    Changed Code around
    Modified how functions are being called

--- Homework4aTesting.py
+++ Homework4aTesting.py
@@ -1,7 +1,5 @@
 import unittest
-import requests
-import json
-import Homework4a
+from Homework4a import getRepositories
 
 class TestHomeworka(unittest.TestCase):
 
@@ -9,13 +7,13 @@ class TestHomeworka(unittest.TestCase):
         #self.assertEqual(Homework4a.getRepositories("mwisnews"), [['BusinessCardReader', 30], ['MWSSW567', 2], ['SSW567GitHubAPI', 16], ['TexasHoldem', 15], ['TriangleTestingAssignment2', 10], ['TriangleTestingClassification', 1], ['VentureHacks-Fintech', 30]])
 
     def testWorkingUsernameB(self):
-        self.assertEqual(Homework4a.getRepositories("richkempinski"), [['hellogitworld', 30], ['helloworld', 6], ['Mocks', 10], ['Project1', 2], ['threads-of-life', 1]])
+        self.assertEqual(getRepositories("richkempinski"), [['hellogitworld', 30], ['helloworld', 6], ['Mocks', 10], ['Project1', 2], ['threads-of-life', 1]])
 
     def testInvalidInputA(self):
-        self.assertEqual(Homework4a.getRepositories(True), "Username must be entered as a string")
+        self.assertEqual(getRepositories(True), "Username must be entered as a string")
     
     def testInvalidInputB(self):
-        self.assertEqual(Homework4a.getRepositories(77), "Username must be entered as a string")
+        self.assertEqual(getRepositories(77), "Username must be entered as a string")
 
 if __name__ == '__main__':
     print('Running unit tests')

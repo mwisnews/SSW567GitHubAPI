c1cac2396a98c85338845629af89176f40127343
mwisnews <mwisnews@stevens.edu>
2020-02-24 19:29:44 -0500

    Updated Travis

--- Homework4aTesting.py
+++ Homework4aTesting.py
@@ -1,4 +1,6 @@
 import unittest
+from nose.tools import assert_true
+import requests
 from Homework4a import getRepositories
 
 class TestHomeworka(unittest.TestCase):
@@ -6,6 +8,8 @@ class TestHomeworka(unittest.TestCase):
     #def testWorkingUsernameA(self):
         #self.assertEqual(Homework4a.getRepositories("mwisnews"), [['BusinessCardReader', 30], ['MWSSW567', 2], ['SSW567GitHubAPI', 16], ['TexasHoldem', 15], ['TriangleTestingAssignment2', 10], ['TriangleTestingClassification', 1], ['VentureHacks-Fintech', 30]])
 
+   # def testWorkingUsernameB(self):
+    #    response = requests.get('')
     def testWorkingUsernameB(self):
         self.assertEqual(getRepositories("richkempinski"), [['hellogitworld', 30], ['helloworld', 6], ['Mocks', 10], ['Project1', 2], ['threads-of-life', 1]])
 
